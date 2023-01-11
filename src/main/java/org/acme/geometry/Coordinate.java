847bb18d3aaa55cca0596340e71d4c0b39d007a6
LSaba <linasaba.18@gmail.com>
2023-01-11 14:57:06 +0100

    Add 0.1 and 0.2 without tests

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,28 @@
 package org.acme.geometry;
 
-public class Coordinate {
+public final class Coordinate {
+    private double x;
+    private double y;
+    public Coordinate(){}
 
+    public Coordinate(double x, double y) {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
---
+++ src/main/java/org/acme/geometry/Point.java
@@ -0,0 +1,21 @@
+package org.acme.geometry;
+
+public class Point implements Geometry{
+
+    private Coordinate coordinate;
+
+    public Point(){};
+
+    public Point(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+}
